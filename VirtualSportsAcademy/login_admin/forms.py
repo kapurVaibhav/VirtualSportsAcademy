b5a495cc5b09e237be5d491f904417410ae66801
VKCSL0010 <VKCSL0010@cspl028.citadel.solution.com>
2015-11-17 18:14:16 +0530

    new changes

--- VirtualSportsAcademy/login_admin/forms.py
+++ VirtualSportsAcademy/login_admin/forms.py
@@ -4,4 +4,5 @@ class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
     print "asdad"
+    print "chnanges"
     keep_logged = forms.BooleanField(required=False, label="Keep me logged in")
