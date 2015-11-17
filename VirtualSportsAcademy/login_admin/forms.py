3e708b919b9fa202011e9513550aaf65a8c42d02
VKCSL0010 <VKCSL0010@cspl028.citadel.solution.com>
2015-11-17 17:56:43 +0530

    changes print statement

--- VirtualSportsAcademy/login_admin/forms.py
+++ VirtualSportsAcademy/login_admin/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
-    print email
+    print "asdad"
     keep_logged = forms.BooleanField(required=False, label="Keep me logged in")
